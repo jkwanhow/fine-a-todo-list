b1abb80b64221359d49ee72778a43f286085e13a
jkwanhow <58902790+jkwanhow@users.noreply.github.com>
2023-02-25 00:27:49 +1100

    installed jest types

---
+++ src/components/exampleComponent/__tests__/Example.spec.tsx
@@ -0,0 +1,8 @@
+import {render, waitFor} from '@testing-library/react';
+import { act } from 'react-dom/test-utils';
+
+import Example from '../Example';
+
+describe('Example Component used to test initial testing', () => {
+
+})
