92f54c9f8d565609a7fd2bbdec170978a1ed5b47
jkwanhow <58902790+jkwanhow@users.noreply.github.com>
2023-02-25 00:44:38 +1100

    built test test

--- src/components/exampleComponent/Example.tsx
+++ src/components/exampleComponent/Example.tsx
@@ -10,7 +10,7 @@ export default function Example({}){
     return(
         <>
         This is a fake component to test tests are working.
-        <div id='clickHolder'>Count is {clicks} </div>
+        <div>Count is <h1 data-testid='number-of-clicks'>{clicks}</h1> </div>
         <button onClick={handleIncrementClick}>Increment</button>
         </>
     )
--- src/components/exampleComponent/__tests__/Example.spec.tsx
+++ src/components/exampleComponent/__tests__/Example.spec.tsx
@@ -1,8 +1,31 @@
-import {render, waitFor} from '@testing-library/react';
+import {render, waitFor, fireEvent} from '@testing-library/react';
 import { act } from 'react-dom/test-utils';
 
 import Example from '../Example';
 
 describe('Example Component used to test initial testing', () => {
 
+    it('should render exactly one button', () => {
+        const {getAllByRole} = render(<Example/>)
+        
+        // Act
+        const buttons = getAllByRole('button');
+
+        // Assert
+        expect(buttons.length).toBe(1);
+    })
+
+    it('should increment the value when button clicked', async () => {
+        const {getByRole, getByTestId} = render(<Example />)
+
+        // Act
+        const initialClicks = Number(getByTestId('number-of-clicks').textContent);
+        const button = getByRole('button');
+        fireEvent.click(button);
+        const postButtonPressClicks = Number(getByTestId('number-of-clicks').textContent);
+        
+        // Assert number of clicks after should be greater than number of clicks before the button
+        // press by one
+        expect(initialClicks - postButtonPressClicks).toBe(1);
+    })
 })
